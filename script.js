175fd997877e7b99bbaf5a8818845caa61386e61
Anuj Kumar <anuj.sot010022@pwioi.com>
2024-12-25 03:43:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('joinBtn').addEventListener('click', function() {
+    document.getElementById('formSection').classList.toggle('hidden');
+});
+
+document.getElementById('jn1').addEventListener('click', function() {
+    document.getElementById('formSection').classList.toggle('hidden');
+});
+document.getElementById('jn2').addEventListener('click', function() {
+    document.getElementById('formSection').classList.toggle('hidden');
+});
+document.getElementById('jn3').addEventListener('click', function() {
+    document.getElementById('formSection').classList.toggle('hidden');
+});
+document.getElementById('jn4').addEventListener('click', function() {
+    document.getElementById('formSection').classList.toggle('hidden');
+});
